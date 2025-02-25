f898e9b5c8c5247e5fd2c842267ca92e58b07ce7
Thay.min <134392865+thayzavi@users.noreply.github.com>
2025-02-25 12:13:06 +0000

    alteração

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -12,14 +12,14 @@ def to_percentagem(series):
 plt.figure(figsize=(15, 5))
 
 # Gráfico de sobrevivência por gênero
-plt.subplot(1, 3, 1)
+plt.subplot(1, 4, 1)
 gender_counts = df.groupby("Sex")["Survived"].mean()
 plt.pie(to_percentagem(gender_counts), labels=gender_counts.index, autopct="%.1f%%", colors=["lightblue", "blue"], startangle=90)
 plt.title("Taxa de Sobrevivência por Gênero")
 
 
 # Gráfico de sobrevivência por classe social
-plt.subplot(1, 3, 2)
+plt.subplot(1, 4, 2)
 sns.barplot(x=df['Pclass'], y=df['Survived'], ci=None, palette="Blues")
 plt.title("Taxa de Sobrevivência por Classe Social")
 plt.xlabel("Classe Social")
@@ -31,13 +31,14 @@ labels = ["Criança", "Adolescente", "Jovem Adulto", "Adulto", "Idoso"]
 df["Faixa Etária"] = pd.cut(df["Age"], bins=bins, labels=labels, right=False)
 
 # Gráfico de sobrevivência por faixa etária
-plt.subplot(1, 3, 3)
+plt.subplot(1, 4, 3)
 sns.barplot(x=df['Faixa Etária'], y=df['Survived'], ci=None, palette="Blues")
 plt.title("Taxa de Sobrevivência por Faixa Etária")
 plt.xlabel("Faixa Etária")
 plt.ylabel("Taxa de Sobrevivência (%)")
 plt.xticks(rotation=45)
 
+plt.subplot(1, 4, 4)
 survival_counts = df["Survived"].value_counts(normalize=True) * 100
 plt.pie(survival_counts, labels=["Não Sobreviveu", "Sobreviveu"], autopct="%.1f%%", colors=["lightblue", "blue"], startangle=90)
 plt.title("Taxa Geral de Sobrevivência")
